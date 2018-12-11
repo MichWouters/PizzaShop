b8d85cf46725958c377bda6f29b1bebb5acf0a7a
Michiel Wouters <mwouters@W3008>
2018-12-11 15:42:54 +0100

    Added Data annotations

--- PizzaShop/ViewModels/PizzaDetailViewModel.cs
+++ PizzaShop/ViewModels/PizzaDetailViewModel.cs
@@ -1,17 +1,24 @@
-﻿namespace PizzaShop.Business.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaShop.Business.Models
 {
     public class PizzaDetailViewModel : Model
     {
         public int PizzaId { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
+        [MaxLength(500)]
         public string Description { get; set; }
 
         public string Image { get; set; }
 
+        [Range(3.99, 9.99)]
         public decimal Price { get; set; }
 
+        [MinLength(2)]
         public string[] Ingredients { get; set; }
 
         public bool IsAvailable { get; set; }
