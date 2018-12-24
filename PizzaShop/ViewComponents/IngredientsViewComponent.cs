f97a25d37e26ed4a852795255826b7ee6ddac187
Michiel Wouters <michiel_woute64@hotmail.com>
2018-12-24 22:53:28 +0100

    Order by Ingredients

--- PizzaShop/ViewComponents/IngredientsViewComponent.cs
+++ PizzaShop/ViewComponents/IngredientsViewComponent.cs
@@ -21,13 +21,16 @@ namespace PizzaShop.ViewComponents
         {
             string MyView = "Ingredients";
             IEnumerable<IngredientModel> ingredientModels = await _service.GetIngredientsAsync();
-            var viewModels = from x in ingredientModels
-                             group x by x.Type into g
-                             select new IngredientViewModel
-                             {
-                                 Category = g.Key,
-                                 Ingredients = g.Where(y => y.Type == g.Key).ToList()
-                             };
+            IEnumerable<IngredientViewModel> viewModels = ingredientModels
+                .GroupBy(model => model.Type)
+                .Select(ingredient => new IngredientViewModel
+                {
+                    Category = ingredient.Key,
+                    Ingredients = ingredient
+                        .Where(y => y.Type == ingredient.Key)
+                        .OrderBy(z => z.Name)
+                        .ToList()
+                });
 
             return View(MyView, viewModels);
         }
--- PizzaShop/ViewModels/CartItem.cs
+++ PizzaShop/ViewModels/CartItem.cs
@@ -1,6 +1,4 @@
-﻿using PizzaShop.Business.Models;
-
-namespace PizzaShop.ViewModels
+﻿namespace PizzaShop.ViewModels
 {
     public class CartViewModel
     {
